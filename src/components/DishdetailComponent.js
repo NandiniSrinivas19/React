c5d528802f97da2c3e0ad6c7637efbc53e818d24
Nandini Srinivas <nandinisrinivas95@gmail.com>
2020-07-22 12:05:31 -0500

    presentational and container component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,7 +19,7 @@ class DishDetail extends Component {
                     
                     <ul key={com.id} className="list-unstyled">
                         <li className="comment"> {com.comment}</li>
-                        <li className="author">--{com.author}, {com.date}</li>
+                        <li className="author">--{com.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(com.date)))}</li>
                     </ul>
 
                 );
@@ -60,7 +60,8 @@ class DishDetail extends Component {
     render()
     {
         return(
-        <div className="row">
+        <div className="container">  
+          <div className="row">
             <div  className="col-12 col-md-5 m-1">
 
                     {this.renderDish(this.props.dish)}
@@ -74,6 +75,8 @@ class DishDetail extends Component {
                 </Card>
 
             </div>
+         </div>
+
         </div>
 
 
